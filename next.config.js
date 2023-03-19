2544babbdb5db8ae8eec98a15dc8d49345adf5f5
albugowy15 <kholidbughowi@gmail.com>
2023-03-19 14:51:58 +0700

    feat: add all files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    dirs: ['src']
+  },
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com']
+  }
 }
 
 module.exports = nextConfig
