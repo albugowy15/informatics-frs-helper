b4e0e79ea3f6969b203306fa486610bbba439d75
albugowy15 <kholidbughowi@gmail.com>
2023-03-19 19:11:44 +0700

    feat: next auth endpoint

---
+++ src/pages/api/[...nextauth].ts
@@ -0,0 +1,28 @@
+import NextAuth from 'next-auth/next';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        const res = await fetch('http://localhost:3000/api/login', {
+          method: 'POST',
+          body: JSON.stringify(credentials),
+          headers: { 'Content-Type': 'application/json' },
+        });
+        const user = await res.json();
+
+        if (res.ok && user) {
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+});
