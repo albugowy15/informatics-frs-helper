fbbb8ec63b4390d5331fe5b2984120c49b6687f7
albugowy15 <kholidbughowi@gmail.com>
2023-03-21 13:40:48 +0700

    feat: login and register page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import Layout from '@/components/Layout';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </Layout>
   );
 }
