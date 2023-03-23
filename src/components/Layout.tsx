683664fe041ef2e0c3f6b4a0fb43077e70261a3b
albugowy15 <kholidbughowi@gmail.com>
2023-03-23 09:32:28 +0700

    feat: add menu navigation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,39 +1,193 @@
+import { Menu } from '@headlessui/react';
+import clsx from 'clsx';
 import { signIn, signOut, useSession } from 'next-auth/react';
+import { useState } from 'react';
+import { AiOutlineHome, AiOutlineSchedule } from 'react-icons/ai';
+import {
+  BsBook,
+  BsFillPersonFill,
+  BsNewspaper,
+  BsPersonCircle,
+} from 'react-icons/bs';
+import { CgMenuGridR } from 'react-icons/cg';
+import { FiExternalLink, FiLogOut } from 'react-icons/fi';
+import { TbArrowsExchange2 } from 'react-icons/tb';
 
 import BasicLink from '@/components/BasicLink';
-import { Button, LinkButton } from '@/components/Button';
+import { Button, IconButton, LinkButton } from '@/components/Button';
+import Dropdown from '@/components/Dropdown';
+import Modal from '@/components/Modal';
 import Typography from '@/components/Typography';
 
 const Navbar = () => {
   const { data: session } = useSession();
+  const [openMenu, setOpenMenu] = useState(false);
 
   return (
-    <header className='w-full bg-white'>
-      <div className='container mx-auto flex items-center justify-between py-2 px-3'>
-        <BasicLink href='/' className='text-base font-bold md:text-xl'>
-          TC FRS Helper
-        </BasicLink>
-        <nav className='flex justify-between gap-2'>
-          {session?.user ? (
-            <>
-              <Typography variant='body1'>{session.user.username}</Typography>
-              <Button variant='filled' onClick={() => signOut()}>
-                Sign Out
-              </Button>
-            </>
-          ) : (
-            <>
-              <Button variant='filled' onClick={() => signIn()}>
-                Login
-              </Button>
-              <LinkButton variant='outlined' href='/register'>
-                Register
-              </LinkButton>
-            </>
-          )}
-        </nav>
-      </div>
-    </header>
+    <>
+      <header className='w-full bg-white'>
+        <div className='container mx-auto flex items-center justify-between py-2 px-3'>
+          <BasicLink href='/' className='text-base font-bold md:text-xl'>
+            TC FRS Helper
+          </BasicLink>
+          <nav className='flex items-center gap-2'>
+            <Button
+              variant='outlined'
+              icon={CgMenuGridR}
+              onClick={() => setOpenMenu(true)}
+            >
+              Menu
+            </Button>
+            {session?.user ? (
+              <>
+                <Dropdown
+                  menuButton={
+                    <IconButton
+                      icon={BsFillPersonFill}
+                      className='h-fit'
+                      size='md'
+                      variant='outlined'
+                    />
+                  }
+                  options={
+                    <>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <BasicLink
+                            className={clsx(
+                              'flex w-full items-center gap-3 rounded px-2 py-1.5 ',
+                              [
+                                active
+                                  ? 'bg-primary-500 text-neutral-50'
+                                  : 'text-neutral-200',
+                              ]
+                            )}
+                            href='/profile'
+                          >
+                            <BsPersonCircle
+                              className={clsx([
+                                active ? 'text-white' : 'text-primary-500',
+                              ])}
+                            />
+                            Profil
+                          </BasicLink>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <BasicLink
+                            className={clsx(
+                              'flex w-full items-center gap-3 rounded px-2 py-1.5 ',
+                              [
+                                active
+                                  ? 'bg-primary-500 text-neutral-50'
+                                  : 'text-neutral-200',
+                              ]
+                            )}
+                            href='/frs'
+                          >
+                            <BsNewspaper
+                              className={clsx([
+                                active ? 'text-white' : 'text-primary-500',
+                              ])}
+                            />
+                            myFRS
+                          </BasicLink>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            onClick={() => signOut()}
+                            className={clsx(
+                              'flex w-full items-center gap-3 rounded px-2 py-1.5 ',
+                              [
+                                active
+                                  ? 'bg-primary-500 text-neutral-50'
+                                  : 'text-neutral-200',
+                              ]
+                            )}
+                          >
+                            <FiLogOut
+                              className={clsx([
+                                active ? 'text-white' : 'text-primary-500',
+                              ])}
+                            />
+                            Logout
+                          </button>
+                        )}
+                      </Menu.Item>
+                    </>
+                  }
+                />
+              </>
+            ) : (
+              <>
+                <Button variant='filled' onClick={() => signIn()}>
+                  Login
+                </Button>
+                <LinkButton variant='outlined' href='/register'>
+                  Register
+                </LinkButton>
+              </>
+            )}
+          </nav>
+        </div>
+      </header>
+      <Modal isOpen={openMenu} setIsOpen={setOpenMenu} title='Menu Navigation'>
+        <div className='py-2'>
+          <Typography variant='body1' className='text-neutral-400'>
+            Berikut daftar menu yang dapat kamu akses
+          </Typography>
+          <div className='flex w-full flex-col divide-y divide-neutral-700 py-2 text-neutral-100'>
+            <BasicLink
+              onClick={() => setOpenMenu(false)}
+              href='/'
+              className='flex items-center gap-3 py-3 hover:text-primary-400'
+            >
+              <AiOutlineHome className='text-lg' />
+              Home
+            </BasicLink>
+            <BasicLink
+              onClick={() => setOpenMenu(false)}
+              href='/panduan'
+              className='flex items-center gap-3 py-3 hover:text-primary-400'
+            >
+              <BsBook className='text-lg' />
+              Panduan
+            </BasicLink>
+            <BasicLink
+              onClick={() => setOpenMenu(false)}
+              className='flex items-center gap-3 py-3 hover:text-primary-400'
+              href='/jadwal'
+            >
+              <AiOutlineSchedule className='text-lg' />
+              Jadwal Matkul
+            </BasicLink>
+            <BasicLink
+              onClick={() => setOpenMenu(false)}
+              className='flex items-center gap-3 py-3 hover:text-primary-400'
+              href='/trading-matkul'
+            >
+              <TbArrowsExchange2 className='text-lg' />
+              Trading Matkul
+            </BasicLink>
+          </div>
+          <div className=' flex items-center justify-between'>
+            <BasicLink
+              href='https://github.com/albugowy15/informatics-frs-helper'
+              className='flex items-center gap-2 text-sm text-neutral-100 hover:text-primary-500'
+            >
+              Source Code{' '}
+              <FiExternalLink className='text-neutral-400 hover:text-primary-500' />
+            </BasicLink>
+            <Button variant='outlined' onClick={() => setOpenMenu(false)}>
+              Close
+            </Button>
+          </div>
+        </div>
+      </Modal>
+    </>
   );
 };
 
---
+++ src/pages/frs/[userId].tsx
@@ -0,0 +1,3 @@
+export default function FRSPage() {
+  return <></>;
+}
