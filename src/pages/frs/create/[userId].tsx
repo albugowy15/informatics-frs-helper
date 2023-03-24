aa485567210b2166c4462dd2d9dabc443b817c71
albugowy15 <kholidbughowi@gmail.com>
2023-03-24 23:37:03 +0700

    feat: create frs plan

--- src/pages/frs/create/[userId].tsx
+++ src/pages/frs/create/[userId].tsx
@@ -1,84 +1,156 @@
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useSession } from 'next-auth/react';
+import { useState } from 'react';
 import {
   Controller,
   FormProvider,
   SubmitHandler,
   useForm,
 } from 'react-hook-form';
+import { toast, Toaster } from 'react-hot-toast';
 import { z } from 'zod';
+
+import { api } from '@/utils/api';
 
 import { Button } from '@/components/Button';
 import { SelectInput, TextInput } from '@/components/Form';
 import Typography from '@/components/Typography';
 
+import ClassPickSection, { ClassResponseData } from '@/ui/frs/ClassPickSection';
+
 const createFRSForm = z.object({
   title: z.string({
     required_error: 'Judul rencana FRS tidak boleh kosong',
   }),
-  semester: z.string({ required_error: 'Semester tidak boleh kosong' }),
-  priority: z
-    .number({ required_error: 'Prioritas tidak boleh kosong' })
-    .min(1)
-    .max(3),
-  matkul: z.array(
-    z.object({
-      id: z.string(),
-    })
-  ),
+  semester: z.number({ required_error: 'Semester tidak boleh kosong' }),
 });
 
 type CreateFRSForm = z.infer<typeof createFRSForm>;
 export default function CreateFRSPage() {
+  const { data: session } = useSession();
   const methods = useForm<CreateFRSForm>({
     resolver: zodResolver(createFRSForm),
+    mode: 'onSubmit',
   });
   const { handleSubmit, control } = methods;
+  const mutatePlan = api.protected.createPlan.useMutation();
   const onSubmit: SubmitHandler<CreateFRSForm> = (data) => {
-    // eslint-disable-next-line no-console
-    console.log(data);
+    if (classTaken.length > 0) {
+      const matkul = classTaken.map((val) => {
+        return { id: val.id };
+      });
+      if (matkul.length == 0) {
+        toast.error('Tidak ada matkul yang diambil');
+        return;
+      }
+      // post pake trpc
+
+      mutatePlan.mutate({
+        title: data.title,
+        semester: data.semester,
+        matkul: matkul,
+        userId: session?.user.id as string,
+      });
+
+      if (mutatePlan.isLoading) {
+        toast.loading('Membuat rencana FRS...');
+        return;
+      }
+      if (mutatePlan.isSuccess) {
+        toast.success('Rencana FRS berhasil dibuat');
+        return;
+      }
+      if (mutatePlan.isError) {
+        toast.error('Gagal membuat rencana FRS: ' + mutatePlan.error.message);
+        return;
+      }
+    }
   };
+  const [classTaken, setClassTaken] = useState<ClassResponseData[]>([]);
 
   return (
     <FormProvider {...methods}>
+      <Toaster />
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Typography variant='h4'>Informasi Plan FRS</Typography>
-        <div className='mt-1 flex items-center justify-between gap-2'>
-          <TextInput
-            label='Judul Plan'
-            name='title'
-            placeholder='Contoh: War PAA'
-          />
-          <Controller
-            control={control}
-            name='semester'
-            render={({ field }) => (
-              <SelectInput
-                placeholder='Pilih Semester'
-                data={[1, 2, 3, 4, 5, 6, 7, 8]}
-                label='Pilih Semester'
-                {...field}
-              />
-            )}
-          />
-          <Controller
-            control={control}
-            name='priority'
-            render={({ field }) => (
-              <SelectInput
-                placeholder='Pilih Prioritas'
-                data={[1, 2, 3]}
-                label='Pilih Prioritas'
-                {...field}
-              />
-            )}
-          />
-        </div>
+        <section className='space-y-2'>
+          <Typography variant='h4'>Informasi Plan FRS</Typography>
+          <div className='w-full md:w-96'>
+            <TextInput
+              label='Judul Plan'
+              name='title'
+              placeholder='Contoh: War PAA'
+            />
+          </div>
+          <div className='w-full md:w-44'>
+            <Controller
+              control={control}
+              name='semester'
+              render={({ field }) => (
+                <SelectInput
+                  placeholder='Pilih Semester'
+                  data={[1, 2, 3, 4, 5, 6, 7, 8]}
+                  label='Pilih Semester'
+                  {...field}
+                />
+              )}
+            />
+          </div>
+        </section>
 
         <div className='py-3' />
-        <Typography variant='h4'>Pilih Kelas</Typography>
-        <Button variant='filled' type='submit'>
+
+        <section className='space-y-2'>
+          <Typography variant='h4'>Matkul yang diambil</Typography>
+          {classTaken.length > 0 ? (
+            <div className='grid grid-cols-4 gap-3'>
+              {classTaken.map((kelas, index) => (
+                <div
+                  key={kelas.id}
+                  className='rounded-md border border-neutral-600 p-3'
+                >
+                  <Typography variant='body1' className='font-medium'>
+                    {kelas.subject} {kelas.code}
+                  </Typography>
+                  <Typography variant='body2'>
+                    {kelas.Lecturer.fullname}
+                  </Typography>
+                  <Typography variant='body2'>
+                    {kelas.day}, {kelas.Session.session_time}
+                  </Typography>
+
+                  <Button
+                    variant='filled'
+                    size='sm'
+                    className='mt-3 bg-error-500 hover:bg-error-400'
+                    onClick={() => {
+                      const classArray = [...classTaken];
+                      classArray.splice(index, 1);
+                      setClassTaken(classArray);
+                    }}
+                  >
+                    Drop
+                  </Button>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <Typography variant='body1' className='text-neutral-400'>
+              Kamu belum mengambil matkul apapun
+            </Typography>
+          )}
+        </section>
+
+        <Button variant='filled' type='submit' className='mt-3'>
           Simpan
         </Button>
+
+        <div className='py-3' />
+
+        <ClassPickSection
+          setClassTaken={setClassTaken}
+          classTaken={classTaken}
+        />
+        <div className='py-3' />
       </form>
     </FormProvider>
   );
---
+++ src/ui/frs/ClassPickSection.tsx
@@ -0,0 +1,174 @@
+import { Dispatch, SetStateAction, useState } from 'react';
+import { toast, Toaster } from 'react-hot-toast';
+
+import { api } from '@/utils/api';
+
+import Accordion from '@/components/Accordion';
+import { Button } from '@/components/Button';
+import { SelectInput } from '@/components/Form';
+import Typography from '@/components/Typography';
+
+export type ClassResponseData = {
+  code: string;
+  id: string;
+  subject: string;
+  Session: {
+    session_time: string;
+  };
+  Lecturer: {
+    code: string;
+    fullname: string;
+  };
+  day: string;
+};
+
+function checkValidPick(
+  listKelas: ClassResponseData[],
+  kelas: ClassResponseData
+): { isValid: boolean; errorMessage: string } {
+  const checkSameClass = listKelas.find((val) => val.subject == kelas.subject);
+  if (checkSameClass) {
+    return {
+      isValid: false,
+      errorMessage: 'Tidak dapat mengambil mata kuliah yang sama',
+    };
+  }
+  const checkClassCollision = listKelas.find(
+    (val) => val.day == kelas.day && val.Session == kelas.Session
+  );
+  if (checkClassCollision) {
+    return {
+      isValid: false,
+      errorMessage:
+        'Tidak dapat mengambil lebih dari satu kelas di jam yang sama',
+    };
+  }
+
+  return { isValid: true, errorMessage: '' };
+}
+
+const ClassPickSection = ({
+  classTaken,
+  setClassTaken,
+}: {
+  classTaken: ClassResponseData[];
+  setClassTaken: Dispatch<SetStateAction<ClassResponseData[]>>;
+}) => {
+  const [semester, setSemester] = useState<number>();
+  const [subject, setSubject] = useState<string>();
+  const listSubject = api.public.getSubject.useQuery(
+    { semester: semester as number },
+    { enabled: Boolean(semester) }
+  );
+  const listClass = api.public.getClass.useQuery(
+    {
+      isAksel: false,
+      semester: semester as number,
+      matkul: subject,
+    },
+    { enabled: Boolean(subject || semester) }
+  );
+
+  return (
+    <section className='space-y-3'>
+      <Toaster />
+      <Typography variant='h4'>Form Ambil Matkul</Typography>
+      <div className='flex items-center gap-3 md:w-[60%]'>
+        <SelectInput
+          label='Pilih Semester'
+          onChange={setSemester}
+          value={semester}
+          data={[1, 2, 3, 4, 5, 6, 7, 8]}
+          placeholder='Pilih Semester'
+        />
+
+        <SelectInput
+          onChange={setSubject}
+          value={subject}
+          label='Pilih Matkul'
+          data={listSubject.data}
+          placeholder='Pilih Matkul'
+        />
+      </div>
+      <div className='flex flex-col gap-2'>
+        {listClass.data ? (
+          <>
+            {listClass.data.map((subject, index) => (
+              <Accordion
+                key={index}
+                title={
+                  <div>
+                    <Typography variant='h5'>{subject.name}</Typography>
+                    <Typography variant='body2'>
+                      Semester {subject.semester} | {subject.sks} sks |{' '}
+                      {subject.Class.length} kelas
+                    </Typography>
+                  </div>
+                }
+              >
+                {subject.Class.length > 0 ? (
+                  <div className='grid grid-cols-4 gap-3'>
+                    {subject.Class.map((kelas) => (
+                      <div
+                        key={kelas.id}
+                        className='rounded-md border border-neutral-600 p-3'
+                      >
+                        <Typography variant='body1' className='font-medium'>
+                          {subject.name} {kelas.code}
+                        </Typography>
+                        <Typography variant='body2'>
+                          {kelas.Lecturer.fullname}
+                        </Typography>
+                        <Typography variant='body2'>
+                          {kelas.day}, {kelas.Session.session_time}
+                        </Typography>
+
+                        <Button
+                          variant='filled'
+                          size='sm'
+                          className='mt-3'
+                          onClick={() => {
+                            const data: ClassResponseData = {
+                              ...kelas,
+                              subject: subject.name,
+                            };
+                            const { isValid, errorMessage } = checkValidPick(
+                              classTaken,
+                              data
+                            );
+                            if (isValid) {
+                              setClassTaken((prev: ClassResponseData[]) => [
+                                ...prev,
+                                data,
+                              ]);
+                              toast.success('Berhasil mengambil kelas');
+                            } else {
+                              toast.error(errorMessage);
+                            }
+                          }}
+                        >
+                          Ambil
+                        </Button>
+                      </div>
+                    ))}
+                  </div>
+                ) : (
+                  <Typography
+                    variant='body2'
+                    className='text-center text-neutral-400'
+                  >
+                    Tidak ada kelas
+                  </Typography>
+                )}
+              </Accordion>
+            ))}
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default ClassPickSection;
