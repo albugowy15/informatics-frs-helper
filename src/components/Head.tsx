0410ae5969da6c88ab3c9c3c89e0a815263c10d9
albugowy15 <kholidbughowi@gmail.com>
2023-04-29 14:18:54 +0700

    fix: meta description self closing tag

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -9,7 +9,7 @@ const HeadElement = ({ title, description }: HeadElementProps) => {
   return (
     <Head>
       <title>{title} - Informatics FRS Helper</title>
-      {description ? <meta name='description'>{description}</meta> : null}
+      {description ? <meta name='description' content={description} /> : null}
     </Head>
   );
 };
