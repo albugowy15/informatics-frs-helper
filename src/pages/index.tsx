0a3caf251b4b3149e2a3e59ebb8a64d77e470f23
albugowy15 <kholidbughowi@gmail.com>
2023-04-30 11:32:44 +0700

    feat: home and report page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,44 @@
-import HeadElement from '@/components/Head';
+import { NextSeo } from 'next-seo';
+import { AiOutlineSchedule } from 'react-icons/ai';
+import { IoNewspaperOutline } from 'react-icons/io5';
+
+import { renderPageTitle } from '@/utils/page';
+
+import { LinkButton } from '@/components/Button';
+import Typography from '@/components/Typography';
 
 export default function Home() {
   return (
     <>
-      <HeadElement title='Home' description='Informatics FRS Helper' />
-      Hallo
+      <NextSeo
+        title={renderPageTitle('Home')}
+        description='Informatics FRS Helper'
+      />
+      <main className='relative mt-28 flex flex-col items-center justify-center space-y-2 '>
+        <Typography variant='h2' className=' text-center text-primary-500'>
+          Informatics FRS Helper
+        </Typography>
+        <Typography variant='body1' className='text-center'>
+          Aplikasi bantuan FRS untuk mahasiswa Informatika ITS
+        </Typography>
+        <div className='py-1' />
+        <div className='flex flex-wrap items-center justify-center gap-2 sm:gap-6'>
+          <LinkButton
+            href='/jadwal'
+            variant='outlined'
+            icon={AiOutlineSchedule}
+          >
+            Lihat Jadwal
+          </LinkButton>
+          <LinkButton
+            href='/panduan'
+            variant='outlined'
+            icon={IoNewspaperOutline}
+          >
+            Baca Panduan
+          </LinkButton>
+        </div>
+      </main>
     </>
   );
 }
--- src/pages/report.tsx
+++ src/pages/report.tsx
@@ -1,3 +1,32 @@
+import { NextSeo } from 'next-seo';
+
+import { renderPageTitle } from '@/utils/page';
+
+import BasicLink from '@/components/BasicLink';
+import Typography from '@/components/Typography';
+
 export default function ReportPage() {
-  return;
+  return (
+    <>
+      <NextSeo
+        title={renderPageTitle('Report')}
+        description='Laporkan dan sarankan fitur untuk Informatics FRS Helper'
+      />
+      <main className='space-y-2'>
+        <Typography variant='h3'>Report</Typography>
+        <Typography variant='body1'>
+          Apabila kamu menemukan bug, jadwal tidak sesuai, atau mau saran buat
+          nambahin fitur, silahkan membuat New Issuee di Repo Github :{' '}
+          <BasicLink
+            newTab
+            className='font-medium text-primary-500'
+            href='https://github.com/albugowy15/informatics-frs-helper'
+          >
+            https://github.com/albugowy15/informatics-frs-helper
+          </BasicLink>
+          . Jika memungkinkan lengkapi juga dengan gambar atau screenshot.
+        </Typography>
+      </main>
+    </>
+  );
 }
