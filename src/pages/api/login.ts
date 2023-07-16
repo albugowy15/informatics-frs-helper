f6636a47c07352fb59cbcda63fdfd8505ed7b609
albugowy15 <kholidbughowi@gmail.com>
2023-07-16 11:52:26 +0700

    refactor: reduce nested code

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -22,70 +22,70 @@ export default async function handler(
   res: NextApiResponse<APIResponse<LoginResponseData>>
 ) {
   const { method } = req;
-  if (method === 'POST') {
-    const { username, password } = req.body;
-    if (!username || !password) {
-      res.status(400).json({
-        status: 'error',
-        message: 'Request missing username or password',
-      });
-      return;
-    }
 
-    const user = await prisma.user.findUnique({
-      where: { username: username },
-    });
-
-    if (!user) {
-      res.status(401).json({
-        status: 'error',
-        message: 'User not found',
-      });
-      return;
-    }
-
-    if (user) {
-      const userId = user.id,
-        userEmail = user.email,
-        userPassword = user.password,
-        userUsername = user.username;
-
-      bcrypt.compare(password, userPassword).then((isMatch) => {
-        if (isMatch) {
-          const payload = {
-            id: userId,
-            username: userUsername,
-            email: userEmail,
-          };
-
-          jwt.sign(payload, KEY, { expiresIn: '1d' }, (_err, token) => {
-            res.status(200).json({
-              status: 'success',
-              message: 'User successfully logged in',
-              data: {
-                id: userId,
-                username: userUsername,
-                email: userEmail,
-                accessToken: token as string,
-              },
-            });
-            return;
-          });
-        } else {
-          res.status(400).json({
-            status: 'error',
-            message: 'Password incorrect',
-          });
-          return;
-        }
-      });
-    }
-  } else {
+  if (method !== 'POST') {
     res.status(400).json({
       status: 'error',
       message: 'Method not implemented',
     });
     return;
+  }
+
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(400).json({
+      status: 'error',
+      message: 'Request missing username or password',
+    });
+    return;
+  }
+
+  const user = await prisma.user.findUnique({
+    where: { username: username },
+  });
+  if (!user) {
+    res.status(401).json({
+      status: 'error',
+      message: 'User not found',
+    });
+    return;
+  }
+
+  const userId = user.id,
+    userEmail = user.email,
+    userPassword = user.password,
+    userUsername = user.username;
+
+  const isMatch = await bcrypt.compare(password, userPassword);
+  if (!isMatch) {
+    res.status(400).json({
+      status: 'error',
+      message: 'Password incorrect',
+    });
+    return;
+  }
+  const payload = {
+    id: userId,
+    username: userUsername,
+    email: userEmail,
+  };
+  try {
+    const token = jwt.sign(payload, KEY, { expiresIn: '1d' });
+    res.status(200).json({
+      status: 'success',
+      message: 'User successfully logged in',
+      data: {
+        id: userId,
+        username: userUsername,
+        email: userEmail,
+        accessToken: token as string,
+      },
+    });
+  } catch (e) {
+    res.status(500).json({
+      status: 'error',
+      message: 'ERROR SIGN TOKEN',
+    });
   }
   return;
 }
--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -13,46 +13,44 @@ export default async function handler(
   res: NextApiResponse<APIResponse<RegisterResponseData>>
 ) {
   const { method } = req;
-  if (method === 'POST') {
-    const { username, password, email, confirmPassword } = req.body;
-
-    // check if password and confirm password match
-    if (password !== confirmPassword) {
-      res.status(400).json({
-        status: 'error',
-        message: 'Password and confirm password do not match',
-      });
-      return;
-    }
-
-    try {
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const user = await prisma.user.create({
-        data: {
-          username: username,
-          email: email,
-          password: hashedPassword,
-        },
-      });
-
-      res.status(201).json({
-        status: 'success',
-        message: 'User successfully registered',
-        data: {
-          id: user.id,
-        },
-      });
-      return;
-    } catch (error) {
-      return res.status(400).json({
-        status: 'error',
-        message: 'Username already exists',
-      });
-    }
-  } else {
+  if (method !== 'POST') {
     res.status(405).json({
       status: 'error',
       message: 'Request method not allowed',
+    });
+    return;
+  }
+  const { username, password, email, confirmPassword } = req.body;
+
+  // check if password and confirm password match
+  if (password !== confirmPassword) {
+    res.status(400).json({
+      status: 'error',
+      message: 'Password and confirm password do not match',
+    });
+    return;
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username: username,
+        email: email,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(201).json({
+      status: 'success',
+      message: 'User successfully registered',
+      data: {
+        id: user.id,
+      },
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: 'error',
+      message: 'Username already exists',
     });
   }
   return;
