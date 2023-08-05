a2b94bc250953e185694d9c10dd33530fd4d7261
albugowy15 <kholidbughowi@gmail.com>
2023-08-05 12:48:32 +0700

    docs: add upmb and pengayaan

--- src/pages/panduan.tsx
+++ src/pages/panduan.tsx
@@ -17,43 +17,32 @@ export default function PanduanPage() {
         </Typography>
         <Typography variant='body1'>
           Website ini diperuntukkan untuk membantu mahasiswa Informatika ITS
-          dalam melakukan FRS. Berikut 3 hal yang bisa kamu lakukan di website
-          ini:
+          dalam melakukan FRS yang terdiri dari 3 fitur sebagai berikut.
         </Typography>
-        <ol className='list-inside list-decimal'>
-          <li>
-            <b>myFRS</b> - Plan atau Rencana FRS
-          </li>
-          <li>
-            <b>myTradeMatkul</b> - Trading Matkul
-          </li>
-          <li>Informasi Jadwal Matkul</li>
-        </ol>
-        <div className='py-1' />
         <Typography variant='h3'>1. myFRS - Plan atau Rencana FRS</Typography>
         <Typography variant='body1'>
-          Kamu bisa membuat dan merancang rencana kelas dan mata kuliah apa saja
-          yang akan kamu ambil saat FRS. Kamu bisa membuat hingga 3 rencana.
-          Setiap kali kamu mengambil matkul akan dilakukan validasi meliputi :
+          Rencanakan kelas dan mata kuliah apa saja yang akan diambil saat FRS.
+          Kuota maksimal plan yang dapat dibuat adalah 3 untuk setiap akun.
+          Setiap kali dilakukan pengambilan kelas, akan dilakukan validasi
+          meliputi :
         </Typography>
         <ul className='list-inside list-disc'>
-          <li>Tidak boleh mengambil kelas yang sama</li>
+          <li>Tidak dapat mengambil kelas yang sama</li>
           <li>
-            Tidak boleh mengambil lebih dari satu kelas pada hari dan jam
+            Tidak dapat mengambil lebih dari satu kelas pada hari dan jam
             perkuliahan yang sama
           </li>
           <li>Tidak dapat mengambil kelas dengan mata kuliah yang sama</li>
           <li>Tidak dapat mengambil lebih dari 24 sks</li>
         </ul>
         <Typography variant='body1'>
-          Dengan ini, rencana FRS yang kamu buat akan tervalidasi dan tidak akan
-          mengambil kelas yang tidak sesuai dengan aturan di atas. Ini juga akan
-          membantu kamu dalam merencanakan FRS dengan lebih matang. Berikut
-          untuk detail cara membuat rencana FRS:
+          Dengan ini, rencana FRS yang dibuat akan valid sehingga membantu
+          perencanaan FRS yang lebih matang. Berikut cara membuat rencana FRS
+          (myFRs):
         </Typography>
         <ol className='list-inside list-decimal'>
           <li>
-            Buka menu <b>myFRS</b>
+            Buka menu <b>myFRS</b> yang ada di ikon Profil
           </li>
           <li>
             Pilih <b>Buat Rencana Baru</b>
@@ -69,50 +58,53 @@ export default function PanduanPage() {
             <b>Simpan</b>.
           </li>
         </ol>
-        <Typography variant='body1'>
-          Kamu juga bisa mengubah atau menghapus rencana FRS yang sudah dibuat.
-        </Typography>
         <div className='py-1' />
         <Typography variant='h3'>2. myTradeMatkul - Trading Matkul</Typography>
         <Typography variant='body1'>
-          Kalau ini kamu bisa gunakan buat nyari orang yang mau tukeran matkul.
-          Kamu bisa lihat daftar postingan trade matkul di menu{' '}
-          <b>Trading Matkul</b>. Untuk membuat postingan trade matkul, ikuti
-          langkah-langkah berikut:
+          Digunakan untuk mencari patner tukar matkul. Silahkan lihat daftar
+          postingan trade matkul di menu <b>Trading Matkul</b>. Untuk membuat
+          postingan trade matkul, ikuti langkah-langkah berikut:
         </Typography>
         <ol className='list-inside list-decimal'>
           <li>
-            Buka menut <b>myTradeMatkul</b>
+            Buka menut <b>myTradeMatkul</b> yang ada di ikon Profil
           </li>
           <li>
-            Pilih semester, nama mata kuliah, dan kode kelas dari kelas yang
-            udah diambil saat FRS
+            Pilih semester, nama mata kuliah, dan kode kelas yang telah diambil
+            saat FRS
           </li>
           <li>
             Kemudian pilih semester, nama mata kuliah, dan kode kelas untuk
-            kelas yang ingin kamu cari
+            kelas yang ingin dicari
           </li>
           <li>Tulis dan lengkapi deskripsinya</li>
           <li>Klik simpan</li>
         </ol>
         <Typography variant='body1'>
-          Sangat dianjurkan agar melengkapi profil dengan mengisi nama lengkap,
-          id line, atau WA untuk memudahkan orang yang mau trade sama kamu. Kamu
-          juga bisa mengubah atau menghapus postingan trade matkul yang sudah
-          dibuat.
+          Sangat dianjurkan agar melengkapi profil terlebih dulu dengan mengisi
+          nama lengkap, id line, atau WA untuk memudahkan patner yang ingin
+          trade dengan kamu. Kamu juga dapat mengubah atau menghapus postingan
+          trade matkul yang telah dibuat.
         </Typography>
         <div className='py-1' />
         <Typography variant='h3'>3. Informasi Jadwal</Typography>
         <Typography variant='body1'>
-          Kamu juga bisa sekedar melihat jadwal setiap matkul dan kelas. Buka
-          aja menu <b>Jadwal Matkul</b>. Kamu bisa lihat jadwal matkul
-          menggunakan filter berdasarkan semester dan nama mata kuliah.
+          Lihat jadwal setiap matkul dan kelas di menu <b>Jadwal Matkul</b>.
+          Filter jadwal berdasarkan semester dan mata kuliah.
         </Typography>
         <div className='py-1' />
         <Typography variant='h2'>Batasan Fitur yang dimiliki</Typography>
         <Typography variant='body1'>
           Terdapat beberapa batasan terkait informasi atau fitur yang ada
           sebagai berikut:{' '}
+        </Typography>
+        <Typography variant='h4'>
+          Tidak menyertakan mata kuliah UPMB dan Pengayaan
+        </Typography>
+        <Typography variant='body1'>
+          Mata kuliah UPMB dan pengayaan tidak disertakan. Hal ini karena mata
+          kuliah UPMB dan pengayaan memiliki jadwal sendiri yang terpisah dari
+          plotting jadwal kelas departemen.
         </Typography>
         <Typography variant='h4'>Maksimal 3 Plan FRS</Typography>
         <Typography variant='body1'>
