dd5ea7ca71b76da1a24c79af0f71fd2aed43fb5b
albugowy15 <kholidbughowi@gmail.com>
2023-10-24 22:52:57 +0700

    style: use basic a tag

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -3,7 +3,6 @@ import { Metadata } from 'next';
 import { renderPageTitle } from '@/utils/page';
 
 import Typography from '@/components/typography';
-import { Button } from '@/components/ui/button';
 
 export const metadata: Metadata = {
   title: renderPageTitle('Report'),
@@ -11,28 +10,30 @@ export const metadata: Metadata = {
 };
 export default function ReportPage() {
   return (
-    <>
-      <main className='space-y-2'>
-        <Typography variant='h3'>Report</Typography>
-        <Typography variant='body1'>
-          Apabila kamu menemukan bug, jadwal tidak sesuai, atau mau saran buat
-          nambahin fitur, silahkan membuat Discussion baru di Repo Github :{' '}
-          <Button variant='link' asChild>
-            <a href='https://github.com/albugowy15/informatics-frs-helper/discussions'>
-              https://github.com/albugowy15/informatics-frs-helper/discussions
-            </a>
-          </Button>
-          . Jika memungkinkan lengkapi juga dengan gambar atau screenshot.
-        </Typography>
-        <Typography variant='body1'>
-          Panduan untuk membuat diskusi dapat dilihat di topik Announcement :{' '}
-          <Button variant='link' asChild>
-            <a href='https://github.com/albugowy15/informatics-frs-helper/discussions/52'>
-              https://github.com/albugowy15/informatics-frs-helper/discussions/52
-            </a>
-          </Button>
-        </Typography>
-      </main>
-    </>
+    <main className='space-y-2'>
+      <Typography variant='h3'>Report</Typography>
+      <Typography variant='body1'>
+        Apabila kamu menemukan bug, jadwal tidak sesuai, atau mau saran buat
+        nambahin fitur, silahkan membuat Discussion baru di Repo Github :{' '}
+        <a
+          className='hover:underline'
+          target='_blank'
+          href='https://github.com/albugowy15/informatics-frs-helper/discussions'
+        >
+          https://github.com/albugowy15/informatics-frs-helper/discussions
+        </a>
+        . Jika memungkinkan lengkapi juga dengan gambar atau screenshot.
+      </Typography>
+      <Typography variant='body1'>
+        Panduan untuk membuat diskusi dapat dilihat di topik Announcement :{' '}
+        <a
+          className='hover:underline'
+          target='_blank'
+          href='https://github.com/albugowy15/informatics-frs-helper/discussions/52'
+        >
+          https://github.com/albugowy15/informatics-frs-helper/discussions/52
+        </a>
+      </Typography>
+    </main>
   );
 }
