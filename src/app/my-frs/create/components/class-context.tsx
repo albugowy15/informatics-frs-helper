ab08ec9aeb235a3c14d5aadde4c31db6505ee03e
albugowy15 <kholidbughowi@gmail.com>
2023-10-26 09:18:01 +0700

    feat: complete create frs page

--- src/app/my-frs/create/components/class-context.tsx
+++ src/app/my-frs/create/components/class-context.tsx
@@ -3,8 +3,8 @@
 import React, { createContext, useState } from 'react';
 
 export const ClassContext = createContext<{
-  classTaken: TakenClassType[] | null;
-  setClassTaken: React.Dispatch<React.SetStateAction<TakenClassType[] | null>>;
+  classTaken: TakenClassType[];
+  setClassTaken: React.Dispatch<React.SetStateAction<TakenClassType[]>>;
 } | null>(null);
 
 export type TakenClassType = {
@@ -28,7 +28,7 @@ export type TakenClassType = {
 };
 
 const ClassContextProvider = ({ children }: { children: React.ReactNode }) => {
-  const [classTaken, setClassTaken] = useState<TakenClassType[] | null>(null);
+  const [classTaken, setClassTaken] = useState<TakenClassType[]>([]);
   return (
     <ClassContext.Provider value={{ classTaken, setClassTaken }}>
       {children}
