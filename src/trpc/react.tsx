125daaf7539e73539b6fb354b1f26a4c7afb66d1
albugowy15 <kholidbughowi@gmail.com>
2023-10-28 12:00:19 +0700

    fix: remove trpc log from browser console

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { loggerLink, unstable_httpBatchStreamLink } from '@trpc/client';
-import { createTRPCReact } from '@trpc/react-query';
-import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { unstable_httpBatchStreamLink } from "@trpc/client";
+import { createTRPCReact } from "@trpc/react-query";
+import { useState } from "react";
 
-import { type AppRouter } from '@/server/api/root';
+import { type AppRouter } from "@/server/api/root";
 
-import { getUrl, transformer } from './shared';
+import { getUrl, transformer } from "./shared";
 
 export const api = createTRPCReact<AppRouter>({
   overrides: {
@@ -30,16 +30,11 @@ export function TRPCReactProvider(props: {
     api.createClient({
       transformer,
       links: [
-        loggerLink({
-          enabled: (op) =>
-            process.env.NODE_ENV === 'development' ||
-            (op.direction === 'down' && op.result instanceof Error),
-        }),
         unstable_httpBatchStreamLink({
           url: getUrl(),
           headers() {
             const heads = new Map(props.headers);
-            heads.set('x-trpc-source', 'react');
+            heads.set("x-trpc-source", "react");
             return Object.fromEntries(heads);
           },
         }),
