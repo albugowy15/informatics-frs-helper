e4fab78ed1d7094c0576c2e5106e7c87616ee27f
albugowy15 <kholidbughowi@gmail.com>
2023-11-07 13:54:32 +0700

    chore: with bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,13 @@
  */
 await import("./src/env.mjs");
 
+import bundleAnalyzer from "@next/bundle-analyzer";
+
 /** @type {import("next").NextConfig} */
 const config = {};
 
-export default config;
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+export default withBundleAnalyzer(config);
