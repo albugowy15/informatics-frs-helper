2c60eca5fcdf2a40d3df39f7a23795789d86a746
albugowy15 <kholidbughowi@gmail.com>
2023-11-14 09:47:25 +0700

    ci: playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests/e2e",
-  timeout: 15 * 1000,
   use: {
     baseURL: "http://localhost:3000",
-    trace: process.env.CI ? "on-first-retry" : "retain-on-failure",
+    trace: "on",
   },
   webServer: {
-    command: "yarn start",
+    command: "npm run start",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
