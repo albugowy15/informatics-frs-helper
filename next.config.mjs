3feee14f4eced2ba7ccb659cb46fecabc144be81
albugowy15 <kholidbughowi@gmail.com>
2023-11-30 13:06:17 +0700

    chore: enable fetch logging

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,13 @@ await import("./src/env.mjs");
 import bundleAnalyzer from "@next/bundle-analyzer";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+};
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
