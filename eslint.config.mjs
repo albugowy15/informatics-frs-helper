3490c8828b832d7d8e35e1f044db60929b075abe
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2025-02-21 22:23:08 +0700

    chore: update deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,14 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
+
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
 });
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default [...compat.extends("next/core-web-vitals", "next/typescript")];
+const eslintConfig = [...compat.extends("next/typescript")];
+
+export default eslintConfig;
