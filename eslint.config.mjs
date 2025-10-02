d2b020d92eaf87ccc2dcdc279bf8f8d1e97b0ab8
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2025-10-03 00:23:14 +0700

    feat: migrate next lint to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
